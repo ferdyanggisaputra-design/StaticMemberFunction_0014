cdbfe1e0c4033e477b6e9fa20cbfe8d26d11fe06
ferdyanggisaputra-design <ferdyanggisaputra@gmail.com>
2026-05-04 12:31:06 +0700

    Menambahkan fungsi main untuk menguji pemanggilan metode setJudul dan getJudul pada kelas buku

--- chainfunctioncall.cpp
+++ chainfunctioncall.cpp
@@ -14,3 +14,13 @@ public:
         return this->judul;
     }
 };
+
+int main() {
+    buku bukunya;
+    
+    // Contoh pemanggilan fungsi berantai (chain function calls)
+    cout << bukunya.setJudul("Matematika").getJudul(); 
+    
+    cout << endl;
+    return 0;
+}
