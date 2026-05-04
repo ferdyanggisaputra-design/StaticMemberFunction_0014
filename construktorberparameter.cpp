2d41ad5af3f99799f643e7543e4441451f080dbe
ferdyanggisaputra-design <ferdyanggisaputra@gmail.com>
2026-05-04 12:27:33 +0700

    Menambahkan definisi konstruktor dan fungsi main untuk kelas mahasiswa

--- construktorberparameter.cpp
+++ construktorberparameter.cpp
@@ -6,3 +6,15 @@ class mahasiswa {
 public:
     mahasiswa(int nim, string nama); // Constructor dengan parameter
 };
+
+// Definisi Constructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama) {
+    cout << "Constructor Dengan Parameter Terpanggil" << endl;
+    cout << "NIM: " << nim << endl;
+    cout << "Nama: " << nama << endl;
+}
+
+int main(){
+    mahasiswa mhs(12345, "Pascal"); // Memanggil Constructor dengan parameter
+    return 0;
+}
